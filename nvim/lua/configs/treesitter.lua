b527d8eb0c54483a1c51710884c2c02ff81ae4c8
Abhijith <abhijithpmenon2004@gmail.com>
2026-02-23 11:37:33 +0530

    initial commit

---
+++ nvim/lua/configs/treesitter.lua
@@ -0,0 +1,28 @@
+local options = {
+    ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "make",
+        "fish",
+        "lua",
+        "luadoc",
+        "markdown",
+        "printf",
+        "python",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+    },
+
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    },
+
+    indent = { enable = true },
+}
+
+require("nvim-treesitter.configs").setup(options)
